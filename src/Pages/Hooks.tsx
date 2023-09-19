51770aba10a2e83492cccf33860f1803f93d7b15
hermy1 <49494077+hermy1@users.noreply.github.com>
2023-09-19 14:52:00 +0000

    add

--- src/Pages/Hooks.tsx
+++ src/Pages/Hooks.tsx
@@ -31,9 +31,9 @@ function Hooks() {
 
   return (
     <div className=''>
-        <h1 className='text-3xl medium'>Hooks</h1>
+      <h1 className='text-3xl medium'>Hooks</h1>
         <div className='m-4'>
-        <input type="text" onChange={onChange} name="name" id='name' ref={inputRef} value={name} className='my-4 g-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 pl-10 p-2.5' />
+        <input type="text" onChange={onChange} name="name" id='name' ref={inputRef} value={name} className='my-4 g-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-1/2 p-2.5' />
         <button className='bg-green-500 py-2 px-4 rounded text-white font-bold' onClick={() => {
             inputRef.current?.focus();
         }}>Focus</button>
@@ -41,11 +41,11 @@ function Hooks() {
         <p className='text-2xl'>counter: {counter}</p>
         </div>
         <div className='m-3'>
-            <button  className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4' onClick={()=> add()}>add</button>
+          <button  className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4' onClick={()=> add()}>add +</button>
             <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4' onClick={()=>reset()}>reset</button>
-            <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4' onClick={()=> subtract()}>subtract</button>
+          <button className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4' onClick={()=> subtract()}>sub -</button>
         </div>
-        <p>{show}</p>
+      <p>{show}</p>
     </div>
   )
 }
